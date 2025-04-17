67819b3b8829963a75e223a178bd2f1f9c2e443d
krishnaindani <indanikrishna@gmail.com>
2025-04-16 19:36:54 -0700

    vars template

--- topics/go/language/variables/exercises/template1/template1.go
+++ topics/go/language/variables/exercises/template1/template1.go
@@ -9,21 +9,37 @@
 // converting the literal value of Pi (3.14).
 package main
 
+import "fmt"
+
 // Add imports
 
 // main is the entry point for the application.
 func main() {
 
 	// Declare variables that are set to their zero value.
+	var a string
+	var b int
+	var c bool
 
 	// Display the value of those variables.
+	fmt.Printf("var a int \t %T [%v]\n", a, a)
+	fmt.Printf("var b string \t %T [%v]\n", b, b)
+	fmt.Printf("var c float64 \t %T [%v]\n", c, c)
 
 	// Declare variables and initialize.
 	// Using the short variable declaration operator.
+	aa := "Hello"
+	bb := 10
+	cc := true
 
 	// Display the value of those variables.
+	fmt.Printf("aa := \"Hello\" \t %T [%v]\n", aa, aa)
+	fmt.Printf("bb := 10 \t %T [%v]\n", bb, bb)
+	fmt.Printf("cc := true \t %T [%v]\n", cc, cc)
 
 	// Perform a type conversion.
+	dd := float64(bb)
 
 	// Display the value of that variable.
+	fmt.Printf("dd := float64(bb) \t %T [%v]\n", dd, dd)
 }
