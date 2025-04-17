fcac1cc0edd745c937807ecfa76b2cb2e4d27679
krishnaindani <indanikrishna@gmail.com>
2025-04-16 20:42:47 -0700

    struct template1

--- topics/go/language/struct_types/exercises/template1/template1.go
+++ topics/go/language/struct_types/exercises/template1/template1.go
@@ -7,17 +7,41 @@
 // Declare and initialize an anonymous struct type with the same three fields. Display the value.
 package main
 
+import "fmt"
+
 // Add imports.
 
 // Add user type and provide comment.
+type user struct {
+	name  string
+	email string
+	age   int
+}
 
 func main() {
 
 	// Declare variable of type user and init using a struct literal.
 
+	u1 := user{
+		name:  "Bob",
+		email: "bob@example.com",
+		age:   20,
+	}
+
 	// Display the field values.
+	fmt.Printf("u1: %+v\n", u1)
 
 	// Declare a variable using an anonymous struct.
+	u2 := struct {
+		name  string
+		email string
+		age   int
+	}{
+		name:  "Dan",
+		email: "dan@example.com",
+		age:   21,
+	}
 
 	// Display the field values.
+	fmt.Printf("u2: %+v\n", u2)
 }
